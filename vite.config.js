2beb9e259775b0581b67c424036887469476a70b
YoriichiGOD <nrgenosop@gmail.com>
2026-03-31 15:22:22 +0530

    Increased Vite Chunk Size limit to silence arbitrary Vercel warnings

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000
+  },
   server: {
     host: '127.0.0.1',
     proxy: {
